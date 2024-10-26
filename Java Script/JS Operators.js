54a52cf8cda05642dc3e115d71a85abac849e8d1
StarcSarath <sarathsharath11@gmail.com>
2024-10-26 15:47:28 +0530

    My Firstb Project

---
+++ Java Script/JS Operators.js
@@ -0,0 +1,39 @@
+// Assignment Operators
+
+let can=15
+can +=20
+console.log(can);
+
+// Arithmetic Operators
+
+let a=40;
+let b=60;
+//console.log(a+b);
+//console.log(a-b);
+//console.log(a*b);
+//console.log(a/b);
+//console.log(a%b);
+//console.log(++a);
+//console.log(--a);
+//console.log(a++);
+//console.log(a);
+//console.log(a--);
+//console.log(a);
+
+// Comparison Operators
+
+let c='40';
+console.log(a<b);
+console.log(a>b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==c);
+console.log(a===c);
+console.log(a==b);
+
+// Logical Operators
+
+console.log(10<20 && 20<30 && 30<40);
+console.log(10<20 || 20>30 || 30>40);
+console.log(!(10>20 || 20>30 || 30>40));
+
